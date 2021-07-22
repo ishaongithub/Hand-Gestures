c0725bc209b7827e36820a1c3ff143e8c73793ff
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-07-22 13:10:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,8 +17,7 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v
 function modelLoaded(){
     console.log("Model Loaded!");
 }
-prediction_1="";
-prediction_2="";
+
 function speak(){
     var synth= window.speechSynthesis;
     speak_data_1="The first prediction is "+prediction_1;
@@ -27,5 +26,61 @@ function speak(){
     synth.speak(utterThis)
 
 }
-
-
+    function check()
+    {
+      img = document.getElementById('captured_image');
+      classifier.classify(img, gotResult);
+    }
+  
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+      if (results[0].label=="good"){
+          document.getElementById("update_emoji").innerHTML="&#128076;";
+      }
+      if (results[0].label=="ok"){
+          document.getElementById("update_emoji").innerHTML="&#128077;";
+      }
+      if (results[0].label=="victory"){
+          document.getElementById("update_emoji").innerHTML="&#9996;";
+      }
+      if (results[0].label=="power to"){
+          document.getElementById("update_emoji").innerHTML="&#9994;";
+      }
+      if (results[0].label=="clap"){
+          document.getElementById("update_emoji").innerHTML="&#128079;";
+      }
+      if (results[0].label=="rock"){
+          document.getElementById("update_emoji").innerHTML="&#129304;";
+      }
+  
+      if (results[1].label=="good"){
+          document.getElementById("update_emoji2").innerHTML="&#128076;";
+      }
+      if (results[1].label=="ok"){
+          document.getElementById("update_emoji2").innerHTML="&#128077;";
+      }
+      if (results[1].label=="victory"){
+          document.getElementById("update_emoji2").innerHTML="&#9996;";
+      }
+      if (results[1].label=="power to"){
+          document.getElementById("update_emoji2").innerHTML="&#9994;";
+      }
+      if (results[1].label=="clap"){
+          document.getElementById("update_emoji2").innerHTML="&#128079;";
+      }
+      if (results[1].label=="rock"){
+          document.getElementById("update_emoji2").innerHTML="&#129304;";
+      }
+  
+  }
+  
+  }
